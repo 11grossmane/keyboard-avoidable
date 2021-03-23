9bf2e50fd9048d3de2725064be5e6c56502c1705
Eric Grossman <eric.josh.grossman@gmail.com>
2021-03-23 10:20:39 -0500

    removed logs

--- src/Avoidable.tsx
+++ src/Avoidable.tsx
@@ -1,82 +1,82 @@
-import * as React from 'react'
+import * as React from "react";
 import {
-    Dimensions,
-    Keyboard,
-    StyleProp,
-    TouchableWithoutFeedback,
-    View,
-    ViewStyle
-} from 'react-native'
+  Dimensions,
+  Keyboard,
+  StyleProp,
+  TouchableWithoutFeedback,
+  View,
+  ViewStyle,
+} from "react-native";
 
-const { width, height } = Dimensions.get('screen')
+const { width, height } = Dimensions.get("screen");
 
 export type AvoidableProps = {
-    containerStyle?: ViewStyle
-    overlayStyle?: ViewStyle
-    focusedItemStyle?: ViewStyle
-}
+  containerStyle?: ViewStyle;
+  overlayStyle?: ViewStyle;
+  focusedItemStyle?: ViewStyle;
+};
 
 export const Avoidable: React.FC<AvoidableProps> = ({
-    children,
-    containerStyle,
-    overlayStyle,
-    focusedItemStyle
+  children,
+  containerStyle,
+  overlayStyle,
+  focusedItemStyle,
 }) => {
-    const [keyboardHeight, setKeyboardHeight] = React.useState(0)
-    const [touchable, setTouchable] = React.useState('')
+  const [keyboardHeight, setKeyboardHeight] = React.useState(0);
+  const [touchable, setTouchable] = React.useState("");
 
-    React.useEffect(() => {
-        Keyboard.addListener('keyboardWillShow', (e) => {
-            setKeyboardHeight(e.endCoordinates.height)
-        })
-        Keyboard.addListener('keyboardWillHide', () => {
-            setKeyboardHeight(0)
-        })
-    }, [])
+  React.useEffect(() => {
+    Keyboard.addListener("keyboardWillShow", (e) => {
+      setKeyboardHeight(e.endCoordinates.height);
+    });
+    Keyboard.addListener("keyboardWillHide", () => {
+      setKeyboardHeight(0);
+    });
+  }, []);
 
-    const getStyle = (str: string): StyleProp<ViewStyle> => {
-        if (keyboardHeight && touchable === str) {
-            return {
-                zIndex: 2,
-                position: 'absolute',
-                width,
-                bottom: keyboardHeight + 20,
-                ...focusedItemStyle
-            }
-        }
-        return {}
+  const getStyle = (str: string): StyleProp<ViewStyle> => {
+    if (keyboardHeight && touchable === str) {
+      return {
+        zIndex: 2,
+        position: "absolute",
+        width,
+        bottom: keyboardHeight + 20,
+        ...focusedItemStyle,
+      };
     }
-    return (
-        <View style={containerStyle}>
-            {!!keyboardHeight && !!touchable && (
-                <View
-                    style={{
-                        zIndex: 1,
-                        position: 'absolute',
-                        top: 0,
-                        left: 0,
-                        backgroundColor: 'rgba(0,0,0,1)',
-                        opacity: 0.5,
-                        height,
-                        width,
-                        ...overlayStyle
-                    }}
-                />
-            )}
-            {React.Children.map(children, (child) => {
-                if (!React.isValidElement(child)) return false
-                if (!child?.key) throw new Error('children keys must be present')
-                let keyStr = child.key.toString()
-                return (
-                    <TouchableWithoutFeedback
-                        onFocus={() => {
-                            setTouchable(keyStr)
-                        }}
-                    >
-                        <View style={[getStyle(keyStr)]}>{child}</View>
-                    </TouchableWithoutFeedback>
-                )
-            })}
-        </View>
-    )
-}
+    return {};
+  };
+  return (
+    <View style={containerStyle}>
+      {!!keyboardHeight && !!touchable && (
+        <View
+          style={{
+            zIndex: 1,
+            position: "absolute",
+            top: 0,
+            left: 0,
+            backgroundColor: "rgba(0,0,0,1)",
+            opacity: 0.5,
+            height,
+            width,
+            ...overlayStyle,
+          }}
+        />
+      )}
+      {React.Children.map(children, (child) => {
+        if (!React.isValidElement(child)) return false;
+        if (!child?.key) throw new Error("children keys must be present");
+        let keyStr = child.key.toString();
+        return (
+          <TouchableWithoutFeedback
+            onFocus={() => {
+              setTouchable(keyStr);
+            }}
+          >
+            <View style={[getStyle(keyStr)]}>{child}</View>
+          </TouchableWithoutFeedback>
+        );
+      })}
+    </View>
+  );
+};
