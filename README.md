cd9ee34070f6083a165ecfa6ecadf2f469379ccc
Eric Grossman <eric.josh.grossman@gmail.com>
2021-03-23 22:06:33 -0500

    styling

--- README.md
+++ README.md
@@ -1,30 +1,26 @@
 # keyboard-avoidable
 
-- avoidable list forms for react native
+-   avoidable list forms for react native
 
 ## Basic Usage
 
 ```javascript
 export default function App() {
-  return (
-    <ScrollView contentContainerStyle={styles.container}>
-      <Avoidable containerStyle={{ flex: 1, width: "100%" }}>
-        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15].map((num) => {
-          return (
-            <TextInput
-              key={num}
-              style={{ width: "90%", alignSelf: "center" }}
-              mode="outlined"
-              placeholder={"placeholder"}
-              label={String(num)}
-            />
-          );
-        })}
-      </Avoidable>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </ScrollView>
-  );
+    return (
+        <Avoidable containerStyle={{ flex: 1, width: '100%' }}>
+            {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15].map((num) => {
+                return (
+                    <TextInput
+                        key={num}
+                        style={{ width: '90%', alignSelf: 'center' }}
+                        mode='outlined'
+                        placeholder={'placeholder'}
+                        label={String(num)}
+                    />
+                )
+            })}
+        </Avoidable>
+    )
 }
 ```
 
--- src/Avoidable.tsx
+++ src/Avoidable.tsx
@@ -1,82 +1,152 @@
-import * as React from "react";
+import * as React from 'react'
 import {
-  Dimensions,
-  Keyboard,
-  StyleProp,
-  TouchableWithoutFeedback,
-  View,
-  ViewStyle,
-} from "react-native";
+    Animated,
+    Dimensions,
+    Keyboard,
+    ScrollView,
+    StyleProp,
+    TouchableOpacity,
+    TouchableWithoutFeedback,
+    View,
+    ViewStyle
+} from 'react-native'
 
-const { width, height } = Dimensions.get("screen");
+const { height: screenHeight, width: screenWidth } = Dimensions.get('screen')
 
 export type AvoidableProps = {
-  containerStyle?: ViewStyle;
-  overlayStyle?: ViewStyle;
-  focusedItemStyle?: ViewStyle;
-};
+    containerStyle?: ViewStyle
+    overlayStyle?: ViewStyle
+    focusedItemStyle?: ViewStyle
+    header?: JSX.Element
+}
+
+let contentOffset = 0
 
 export const Avoidable: React.FC<AvoidableProps> = ({
-  children,
-  containerStyle,
-  overlayStyle,
-  focusedItemStyle,
+    children,
+    containerStyle,
+    overlayStyle,
+    focusedItemStyle,
+    header
 }) => {
-  const [keyboardHeight, setKeyboardHeight] = React.useState(0);
-  const [touchable, setTouchable] = React.useState("");
+    const [keyboardHeight, setKeyboardHeight] = React.useState(0)
+    const [touchable, setTouchable] = React.useState('')
+    let [animated, setAnimated] = React.useState(new Animated.Value(0))
+    const [layoutMap, setLayoutMap] = React.useState<{
+        [index: string]: { y: number }
+    }>({})
 
-  React.useEffect(() => {
-    Keyboard.addListener("keyboardWillShow", (e) => {
-      setKeyboardHeight(e.endCoordinates.height);
-    });
-    Keyboard.addListener("keyboardWillHide", () => {
-      setKeyboardHeight(0);
-    });
-  }, []);
+    React.useEffect(() => {
+        let sub1 = Keyboard.addListener('keyboardWillShow', (e) => {
+            console.log('coords', e.startCoordinates.height)
+            setKeyboardHeight(e.endCoordinates.height)
+        })
+        let sub2 = Keyboard.addListener('keyboardWillHide', () => {
+            setKeyboardHeight(0)
+        })
+        return () => {
+            sub1.remove()
+            sub2.remove()
+        }
+    }, [])
 
-  const getStyle = (str: string): StyleProp<ViewStyle> => {
-    if (keyboardHeight && touchable === str) {
-      return {
-        zIndex: 2,
-        position: "absolute",
-        width,
-        bottom: keyboardHeight + 20,
-        ...focusedItemStyle,
-      };
+    const move = (str: string) => {
+        let itemHeight = layoutMap[str].y
+        let an = new Animated.Value((itemHeight / screenHeight) * 2.5)
+        Animated.timing(an, {
+            duration: 400,
+            toValue: 1,
+            useNativeDriver: true
+        }).start()
+        return an
     }
-    return {};
-  };
-  return (
-    <View style={containerStyle}>
-      {!!keyboardHeight && !!touchable && (
-        <View
-          style={{
-            zIndex: 1,
-            position: "absolute",
-            top: 0,
-            left: 0,
-            backgroundColor: "rgba(0,0,0,1)",
-            opacity: 0.5,
-            height,
-            width,
-            ...overlayStyle,
-          }}
-        />
-      )}
-      {React.Children.map(children, (child) => {
-        if (!React.isValidElement(child)) return false;
-        if (!child?.key) throw new Error("children keys must be present");
-        let keyStr = child.key.toString();
-        return (
-          <TouchableWithoutFeedback
-            onFocus={() => {
-              setTouchable(keyStr);
+
+    const getStyle = (str: string): StyleProp<any> => {
+        if (keyboardHeight && touchable === str) {
+            console.log('content', screenHeight - contentOffset)
+            return {
+                zIndex: 2,
+                position: 'absolute',
+                width: screenWidth * 0.96,
+                left: screenWidth * 0.02,
+
+                top: contentOffset + (screenHeight - keyboardHeight) - 165,
+                transform: [
+                    {
+                        translateY: animated.interpolate({
+                            inputRange: [0, 1],
+                            outputRange: [-180, 0]
+                        })
+                    }
+                ],
+                ...focusedItemStyle
+            }
+        }
+        return {}
+    }
+    return (
+        <ScrollView
+            overScrollMode='always'
+            scrollToOverflowEnabled={false}
+            contentContainerStyle={{
+                padding: 10,
+                minHeight: '100%',
+                paddingBottom: 80,
+                ...containerStyle
             }}
-          >
-            <View style={[getStyle(keyStr)]}>{child}</View>
-          </TouchableWithoutFeedback>
-        );
-      })}
-    </View>
-  );
-};
+            style={{ flex: 1 }}
+            scrollEventThrottle={8}
+            onScroll={(e) => {
+                contentOffset = e.nativeEvent.contentOffset.y
+            }}
+        >
+            {header && header}
+            {!!keyboardHeight && !!touchable && (
+                <View
+                    style={{
+                        zIndex: 1,
+                        position: 'absolute',
+                        top: 0,
+                        left: 0,
+                        backgroundColor: 'rgba(0,0,0,1)',
+                        opacity: 0.5,
+                        minHeight: 100000, //anything larger than scrollview
+                        width: screenWidth,
+                        ...overlayStyle
+                    }}
+                >
+                    <TouchableOpacity style={{ height: '100%' }} onPress={Keyboard.dismiss} />
+                </View>
+            )}
+
+            {React.Children.map(children, (child) => {
+                if (!React.isValidElement(child)) return false
+                if (!child?.key) return <View>{child}</View>
+                let keyStr = child.key.toString()
+                return (
+                    <TouchableWithoutFeedback
+                        onFocus={() => {
+                            setTouchable(keyStr)
+                            let an = move(keyStr)
+                            setAnimated(an)
+                        }}
+                    >
+                        <Animated.View
+                            onLayout={(e) => {
+                                console.log(e.nativeEvent.layout.y, e.nativeEvent.layout.height)
+                                if (!layoutMap[keyStr])
+                                    setLayoutMap({
+                                        ...layoutMap,
+                                        [keyStr]: { y: e.nativeEvent.layout.y }
+                                    })
+                            }}
+                            style={[getStyle(keyStr)]}
+                        >
+                            {child}
+                        </Animated.View>
+                    </TouchableWithoutFeedback>
+                )
+            })}
+        </ScrollView>
+    )
+}
